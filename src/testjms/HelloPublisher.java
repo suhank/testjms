af7ecdcb1682e7cdced8e17072f01b158934b64f
feisishui <feisishui@feisishui-ThinkPad-X220>
2016-01-25 17:01:08 +0800

    test

--- src/testjms/HelloPublisher.java
+++ src/testjms/HelloPublisher.java
@@ -23,7 +23,7 @@ public class HelloPublisher {
 	public HelloPublisher(String factoryJNDI, String topicJNDI)
 
 	throws JMSException, NamingException {
-
+	   //test
 		Hashtable props = new Hashtable();
 
 		props.put(Context.INITIAL_CONTEXT_FACTORY,
